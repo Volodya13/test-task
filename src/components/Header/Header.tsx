880516a20e515cd484f29097bd8e57d7e98442d7
Vladimir Maslenkov <maslenkov.vladimir@gmail.com>
2025-02-27 22:26:08 +0500

    feat: added search-form ui and implemented Header to the app

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,12 @@
 import styles from './Header.module.css';
-import SearchBtnIcon  from "../../assets/search-btn-icon.svg"
+import SearchForm from "../../features/SearchForm/SearchForm.tsx";
 
 
 function Header() {
   return (
     <header className={styles.header}>
       <h1>Dashboard</h1>
-      <form className={styles.search}>
-        <input type={"search"} placeholder={"What test are you looking for?"}/>
-        <button type={"submit"}>
-          <SearchBtnIcon/>
-        </button>
-      </form>
+      <SearchForm/>
     </header>
   )
 }
---
+++ src/features/SearchForm/SearchForm.tsx
@@ -0,0 +1,15 @@
+import styles from './SearchForm.module.css';
+import SearchBtnIcon from "../../assets/search-btn-icon.svg";
+
+function SearchForm() {
+  return (
+    <form className={styles.searchForm}>
+      <input className={styles.searchField} type={"search"} placeholder={"What test are you looking for?"}/>
+      <button className={styles.searchBtn} type={"submit"}>
+        <SearchBtnIcon/>
+      </button>
+    </form>
+  )
+}
+
+export default SearchForm;
